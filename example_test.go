428ee895f8cefc1dbb97541b712d8ec2002c8a33
Bas Westerbaan <bas@westerbaan.name>
2018-02-26 13:51:42 +0100

    initial commit

---
+++ example_test.go
@@ -0,0 +1,24 @@
+package pow_test
+
+import (
+	"fmt"
+	"github.com/bwesterb/go-pow" // imported as pow
+)
+
+func Example() {
+	// Create a proof of work request with difficulty 5
+	req := pow.NewRequest(5, []byte("some random nonce"))
+	fmt.Printf("req:   %s\n", req)
+
+	// Fulfil the proof of work
+	proof, _ := pow.Fulfil(req, []byte("some bound data"))
+	fmt.Printf("proof: %s\n", proof)
+
+	// Check if the proof is correct
+	ok, _ := pow.Check(req, proof, []byte("some bound data"))
+	fmt.Printf("check: %v", ok)
+
+	// Output: req:   sha2bday-5-c29tZSByYW5kb20gbm9uY2U
+	// proof: AAAAAAAAAAMAAAAAAAAABAAAAAAAAAAI
+	// check: true
+}
